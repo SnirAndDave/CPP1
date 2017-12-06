72671f4edc0396799b5ec9a8d1b8a0d18c9a5fc9
Snir Haphiloni <t-snhaph@microsoft.com>
2017-12-06 19:44:38 +0200

    Redesign

---
+++ HW1/HW1/ISolver.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+#ifndef __ISOLVER_H_INCLUDED__
+#define __ISOLVER_H_INCLUDED__   
+#include <vector>
+#include "Element.h"
+
+class ISolver
+{
+public:
+	virtual ~ISolver() = default;
+	virtual bool solve(std::pair<int, int>& dimentions, std::vector<std::vector<Element>>& puzzle,
+	                   std::vector<Element>& remaining_elements) = 0;
+};
+
+class RecursiveSolver : public ISolver
+{
+public:
+
+	bool solve(std::pair<int, int>& dimentions, std::vector<std::vector<Element>>& puzzle,
+		std::vector<Element>& remaining_elements) override;
+};
+#endif
---
+++ HW1/HW1/RecursiveSolver.cpp
@@ -0,0 +1,51 @@
+#include "Puzzle.h"
+#include "BaseSolver.h"
+
+#include <sstream>
+#include <algorithm>
+
+using namespace std;
+
+class RecursiveSolver : public BaseSolver
+{
+public:
+
+	bool solve(pair<int, int>& dimentions, vector<vector<Element>>& puzzle,
+	           vector<Element>& remaining_elements) override
+	{
+		return rec_solve(0, 0, dimentions, puzzle, remaining_elements);
+	}
+
+private:
+
+	bool rec_solve(int r, int c, pair<int, int>& dimensions, vector<vector<Element>>& mat,
+	               vector<Element>& remaining_elements)
+	{
+		if (remaining_elements.empty() || r == dimensions.first)
+		{
+			return true;
+		}
+		for (Element remaining_element : remaining_elements)
+		{
+			if (!can_be_placed(r, c, dimensions, mat, remaining_element))
+			{
+				continue;
+			}
+			mat[r][c] = remaining_element;
+			vector<Element> remaining_elements_copy = remaining_elements;
+			remaining_elements_copy.erase(
+				remove(remaining_elements_copy.begin(), remaining_elements_copy.end(), remaining_element),
+				remaining_elements_copy.end()); // remove the element we placed in the puzzle from the remaining elements
+			int next_c = (c + 1) % dimensions.second; // end of line
+			int next_r = next_c == 0 ? r + 1 : r;
+			if (rec_solve(next_r, next_c, dimensions, mat, remaining_elements_copy))
+			{
+				return true;
+			}
+			mat[r][c] = Element();
+		}
+		return false;
+	}
+
+	
+};
