64a607bb5752e8126031a4d0f7e4232a37c9de46
Snir Haphiloni <t-snhaph@microsoft.com>
2017-12-24 18:37:27 +0200

    Multi threaded version

--- HW1/HW1/Puzzle.h
+++ HW1/HW1/Puzzle.h
@@ -5,6 +5,9 @@
 #include <vector>
 #include <memory>
 #include "BaseSolver.h"
+#include <thread>
+#include <mutex>
+#include <atomic>
 
 using namespace std;
 
@@ -19,15 +22,16 @@ enum Corner
 class Puzzle
 {
 public:
-	Puzzle(ofstream& fout, bool rotation)
-		: _is_rotation_enabled(rotation), _m_fout(fout)
+	Puzzle(ofstream& fout, bool rotation, int thread_cnt)
+		: _is_rotation_enabled(rotation), _fout(fout), _finished(false), _solved(false)
 	{
 		_size = 0;
+		_thread_cnt = thread_cnt > 9 ? 9 : thread_cnt;
 	}
 
 	void print_solution(const vector<vector<Element>>& matrix) const;
 	static vector<vector<Element>> create_empty_mat(const pair<int, int>& dimensions);
-	unique_ptr<BaseSolver> choose_solver();
+	vector<shared_ptr<BaseSolver>> choose_solver();
 	void solve();
 	static Element get_element(const vector<vector<Element>>& mat, int r, int c);
 	static void print_solution_to_console(const vector<vector<Element>>& matrix);
@@ -35,16 +39,23 @@ public:
 	int _size;
 	bool _is_rotation_enabled;
 private:
+	void set_solution(const vector<vector<Element>>& mat, bool solved);
+	void thread_solve(pair<int, int> row_col_pair, const shared_ptr<BaseSolver>& solver);
 	vector<pair<int, int>> size_to_matrices() const;
 	int get_straight_edges_count();
-	vector<pair<int, int>> get_valid_dimensions(vector<pair<int, int>> dimentions); // straight edges
-	vector<vector<vector<Element>>> create_all_permutations_of_dimension(pair<int, int> dimentions) const;
-	static vector<vector<Element>> vector_to_mat(vector<Element> copy, pair<int, int> dimentions);
+	vector<pair<int, int>> get_valid_dimensions(vector<pair<int, int>> dimensions); // straight edges
+	vector<vector<vector<Element>>> create_all_permutations_of_dimension(pair<int, int> dimensions) const;
+	static vector<vector<Element>> vector_to_mat(vector<Element> copy, pair<int, int> dimensions);
 	static bool verify_matrix(vector<vector<Element>> mat);
 	vector<Corner> find_missing_corners();
 	bool validate_sum_of_edges();
-	
-	ofstream& _m_fout;
+	int _thread_cnt;
+	ofstream& _fout;
+	mutex _mutex;
+	condition_variable _cv;
+	bool _finished;
+	bool _solved;
+	vector<vector<Element>> _solution;
 };
 
 
