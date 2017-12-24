4c50869239b3f6cc6c6ef112cdde9b39a4a0b0de
lederdavid <lederkremer@gmail.com>
2017-12-24 21:15:10 +0200

    created new recursive solver classes. not done

---
+++ HW1/HW1/TopRightRecursiveSolver.h
@@ -0,0 +1,21 @@
+﻿#pragma once
+#ifndef __RECURSIVESOLVER_H_INCLUDED__
+#define __RECURSIVESOLVER_H_INCLUDED__   
+#include "BaseSolver.h"
+
+class TopRightRecursiveSolver :
+	public BaseSolver
+{
+public:
+	TopRightRecursiveSolver();
+	~TopRightRecursiveSolver();
+	bool solve(pair<int, int>& dimentions, vector<vector<Element>>& puzzle,
+		vector<Element>& remaining_elements) override;
+protected:
+	void sort_elements(vector<Element>& elements) override;
+
+private:
+	bool rec_solve(int r, int c, pair<int, int>& dimensions, vector<vector<Element>>& mat,
+		vector<Element>& remaining_elements);
+};
+#endif
